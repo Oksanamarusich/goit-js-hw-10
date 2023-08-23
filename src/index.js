dbf4f3a62f79694d837071fc4bc074c4c750be74
Oksanamarusich <oksana.m229@gmail.com>
2023-08-23 14:12:29 +0300

    add css and libraries

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import axios from "axios";
 //import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 axios.defaults.headers.common["x-api-key"] = "live_8aZbemLZIhFuazU7fpJCOOdB1PDhnbA1NaeZdUVRA8e4VHqiMPSJ8p5Axeu3hqO1";
 
-
-
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const elem = {
     select: document.querySelector('.breed-select'),
@@ -11,17 +12,27 @@ const elem = {
     loader: document.querySelector('.loader'),
      error: document.querySelector('.error'),
 } 
+ const slimSelect = new SlimSelect({
+     select: elem.select,
+ })
+
+//elem.error.style.visibility = "hidden";
+//elem.error.style.visibility = "visible";
+
+Notify.failure('Oops! Something went wrong! Try reloading the page!');
+
 elem.select.addEventListener('change', onChange);
 
-function onChange(event) {
-    fetch(breeds);
-    //event.currentTarget.value
+function onChange() {
+    
+
+    
 
     
 }
 
-fetch('https://api.thecatapi.com/v1/breeds')
-    .then(resp => {
+fetch('https://api.thecatapi.com/v1/breeds') 
+   .then(resp => {
         return resp.json;
     })
     .then(breeds => {
@@ -29,4 +40,7 @@ fetch('https://api.thecatapi.com/v1/breeds')
     })
     .catch(error => {
     console.log(error);
-})
+    })
+ 
+
+    
