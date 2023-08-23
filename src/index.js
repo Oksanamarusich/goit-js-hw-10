dbfc30917c9b2e6f8160f5eefcbd76e9fa1b0644
Oksanamarusich <oksana.m229@gmail.com>
2023-08-23 23:03:05 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import axios from "axios";
-//import { fetchBreeds, fetchCatByBreed } from "./cat-api";
-axios.defaults.headers.common["x-api-key"] = "live_8aZbemLZIhFuazU7fpJCOOdB1PDhnbA1NaeZdUVRA8e4VHqiMPSJ8p5Axeu3hqO1";
+
 
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchBreeds } from "./cat-api";
 
 const elem = {
     select: document.querySelector('.breed-select'),
@@ -18,29 +17,61 @@ const elem = {
 
 //elem.error.style.visibility = "hidden";
 //elem.error.style.visibility = "visible";
+elem.loader.style.visibility = "hidden";
+//Notify.failure('Oops! Something went wrong! Try reloading the page!');
 
-Notify.failure('Oops! Something went wrong! Try reloading the page!');
-
+fetchBreeds();
 elem.select.addEventListener('change', onChange);
 
 function onChange() {
+   
     
 
-    
 
     
 }
 
-fetch('https://api.thecatapi.com/v1/breeds') 
-   .then(resp => {
-        return resp.json;
-    })
-    .then(breeds => {
-        console.log(breeds);
-    })
-    .catch(error => {
-    console.log(error);
-    })
+
+
+
+    
+    
+    
+
+
+
+//     // const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+    
+//     // const API_KEY = 'live_8aZbemLZIhFuazU7fpJCOOdB1PDhnbA1NaeZdUVRA8e4VHqiMPSJ8p5Axeu3hqO1';
+//     // return fetch(BASE_URL).then((resp) =>
+//     //     // if (!resp.ok) {
+//     //     //     throw new Error(resp.statusText);
+//     //     // }
+//     //     resp.json())
+//     //     .then(data => data.map(breed => ({ id: breed.id, name: breed.name })))
+//     //     .catch(error => {
+//     //         console.error('Помилка', error);
+//     //         return [];
+//     //     });
+// }
+    
+
+    
+//         // .then(breeds => {
+//         //     console.log(breeds);
+//         // });
+
+
+// // fetch('https://api.thecatapi.com/v1/breeds') 
+// //    .then(resp => {
+// //         return resp.json;
+// //     })
+// //     .then(breeds => {
+// //         console.log(breeds);
+// //     })
+// //     .catch(error => {
+// //     console.log(error);
+// //     })
  
 
     
