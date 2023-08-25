6e3abbf138df7332ec20a9de7275a8e62f0d662f
Oksanamarusich <oksana.m229@gmail.com>
2023-08-25 13:32:18 +0300

    add

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,7 +1,7 @@
 
 const url = 'https://api.thecatapi.com/v1';
 const end_point = '/breeds';
-const api_key = 'live_8aZbemLZIhFuazU7fpJCOOdB1PDhnbA1NaeZdUVRA8e4VHqiMPSJ8p5Axeu3hqO1';
+const api_key = 'live_q2UOY4s0dIViJHeJ7pAcyDg3qz2DWjVSwrpeDNpBfdmY9MuVyUhyMLubA5l9R5nP';
 
 export function fetchBreeds() {
     return fetch(`${url}${end_point}?api-key = ${api_key}`)
@@ -18,6 +18,7 @@ export function fetchBreeds() {
 };
 
 export function fetchCatByBreed(breedId) {
+    console.log(breedId);
     return fetch(`${url}/images/search?api-key = ${api_key}&breed_ids=${breedId}`)
         .then((resp) => {
             console.log(resp);
--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ const elem = {
 elem.error.classList.add('is-hidden');
 elem.loader.classList.add('is-hidden');
 elem.loader.classList.replace('loader', 'is-hidden');
+elem.cardCatInfo.classList.add('is-hidden');
 
 fetchBreeds().then(data => {
     const breeds = data.map(breed => ({
@@ -40,46 +41,62 @@ fetchBreeds().then(data => {
 elem.select.addEventListener('change', onChange);
 
 function onChange(evt) {
-    const breedId = evt.currentTarget.value;
-    console.log(breedId);
+    elem.loader.classList.replace('is-hidden', 'loader');
+   
+    console.log(evt.currentTarget);
+     const breedId = evt.currentTarget.value;
+     console.log(breedId);
     
     fetchCatByBreed(breedId)
         .then(data => {
-            console.log(data)
-            const { url, breeds } = data[0]
-            console.log(data[0])
-            elem.cardCatInfo.innerHTML = createMarcup(data.url, breeds);
-             
-        })
+            console.log(data);
+            const {url} = data[0];
+            console.log(data[0]);
+
+            
+            const { breeds} = data[0];
+            console.log(breeds);
+            const { name, description, temperament } = breeds;
+            
+            elem.cardCatInfo.innerHTML = `<img src ="${ url }", alt = "",</><h1></h1><p></p><h2>Temperament</h2><p></p>`
+    })
         .catch(err => {
+            
             Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
                 position: 'center-top',
         
             });
-        });
+         });
         
            
         
  
-//     //elem.loader.classList.replace('is-hidden', 'loader');
-//     //elem.select.classList.add('is-hidden');
+elem.loader.classList.replace('loader', 'is-hidden');
+elem.select.classList.add('is-hidden');
 
 
     
  }
 
-function createMarcup(arr) {
-    return arr.map(({url, breeds}) => `<img src = "${url}", alt = "${breeds[0].name}" width = "400"></>
-    <h1>${breeds[0].name}</h1>
-    <p>${breeds[0].description}</p>
-    <h2>Temperament:</h2>
-    <p>${breeds[0].temperament}</p>`).join('')
+// function createMarcup(arr) {
+//     return arr.map(({url, breeds: {name,description,temperament}}) => `<img src = "${url}", alt = "${breeds[0].name}" width = "400"></>
+//     <h1>${breeds[0].name}</h1>
+//     <p>${breeds[0].description}</p>
+//     <h2>Temperament:</h2>
+//     <p>${breeds[0].temperament}</p>`).join('')
 
-}
- createMarcup()
+// }
+ //createMarcup()
 
     
-    
+    // elem.loader.classList.replace('loader', 'is-hidden');
+    //     elem.select.classList.remove('is-hidden');
+    //         const { url } = data[0];
+    //         console.log(data[0]);
+    //         const { name, description, temperament } = data[0].breeds[0];
+        
+    //     elem.cardCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${name}" width="400"/></div><div class="box"><h1>${name}</h1><p>${description}</p><p><b>Temperament:</b> ${temperament}</p></div>`
+    //     elem.cardCatInfo.classList.remove('is-hidden');
     
 
 
