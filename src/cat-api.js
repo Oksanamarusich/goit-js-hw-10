5d33ab78a1c5f147396e71bb8ad834ac3d5784d1
Oksanamarusich <oksana.m229@gmail.com>
2023-08-25 22:15:49 +0300

    add

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,32 +1,35 @@
+import axios from "axios";
+axios.defaults.headers.common["x-api-key"] = "live_q2UOY4s0dIViJHeJ7pAcyDg3qz2DWjVSwrpeDNpBfdmY9MuVyUhyMLubA5l9R5nP";
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
 
-const url = 'https://api.thecatapi.com/v1';
-const end_point = '/breeds';
-const api_key = 'live_q2UOY4s0dIViJHeJ7pAcyDg3qz2DWjVSwrpeDNpBfdmY9MuVyUhyMLubA5l9R5nP';
+const url = 'https://api.thecatapi.com/v1/';
 
-export function fetchBreeds() {
-    return fetch(`${url}${end_point}?api-key = ${api_key}`)
-        .then((resp) => {
-            console.log(resp);
-            if (!resp.ok) {
-                throw new Error(resp.statusText);
-            }
 
-            return resp.json();
-            
+ function fetchBreeds() {
+    return axios('breeds')
+        .then(({data}) => {
+            console.log(data);
+            return data;
         })
-    
+        .catch(error => {
+            console.log(error)
+            // Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
+            //      position: 'center-top')
+    })
 };
 
-export function fetchCatByBreed(breedId) {
-    console.log(breedId);
-    return fetch(`${url}/images/search?api-key = ${api_key}&breed_ids=${breedId}`)
-        .then((resp) => {
-            console.log(resp);
-            if (!resp.ok) {
-                throw new Error(resp.statusText);
-            }
-
-            return resp.json();
-            
+ function fetchCatByBreed(breedId) {
+    return axios.get(`${url}images/search?breed_ids=${breedId}`)
+        .then(({data}) => {
+            console.log(data);
+            return data;
         })
+        .catch(error => {
+            console.log(error)
+            // Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
+            //      position: 'center-top')
+    })
 };
+
+
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
-
+import axios from "axios";
+axios.defaults.headers.common["x-api-key"] = "live_q2UOY4s0dIViJHeJ7pAcyDg3qz2DWjVSwrpeDNpBfdmY9MuVyUhyMLubA5l9R5nP";
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
@@ -18,22 +19,27 @@ elem.error.classList.add('is-hidden');
 elem.loader.classList.add('is-hidden');
 elem.loader.classList.replace('loader', 'is-hidden');
 elem.cardCatInfo.classList.add('is-hidden');
+//elem.cardCatInfo.classList.replace('cat-info', 'is-hidden');
 
 fetchBreeds().then(data => {
-    const breeds = data.map(breed => ({
-        value: breed.id, text: breed.name
-    }));
-      new SlimSelect({
-    select: elem.select,
-    data: breeds,
- });
     
-    })
-   .catch(err => {
+     const breeds = data.map(breed => ({
+         value: breed.id, text: breed.name
+     }));
+       new SlimSelect({
+     select: elem.select,
+     data: breeds,
+  });
+})
+     .catch(err => {
+       console.log(err);
       Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
         position: 'center-top',
         
-}); 
+      })
+          .finally(() => {
+             
+         })
 });
         
 
@@ -41,63 +47,29 @@ fetchBreeds().then(data => {
 elem.select.addEventListener('change', onChange);
 
 function onChange(evt) {
-    elem.loader.classList.replace('is-hidden', 'loader');
-   
-    console.log(evt.currentTarget);
-     const breedId = evt.currentTarget.value;
-     console.log(breedId);
     
+     const breedId = evt.currentTarget.value;
     fetchCatByBreed(breedId)
         .then(data => {
-            console.log(data);
-            const {url} = data[0];
-            console.log(data[0]);
-
-            
-            const { breeds} = data[0];
-            console.log(breeds);
-            const { name, description, temperament } = breeds;
-            
-            elem.cardCatInfo.innerHTML = `<img src ="${ url }", alt = "",</><h1></h1><p></p><h2>Temperament</h2><p></p>`
-    })
+           
+            const { url } = data[0];
+            const { name, description, temperament } = data[0].breeds[0];
+            elem.cardCatInfo.classList.remove('is-hidden');
+            elem.cardCatInfo.innerHTML = `<div><img src ="${url}", alt = "${name}", width ="800"</></div>
+            <div><h1>${name}</h1>
+            <p>${description}</p>
+            <h2>Temperament</h2>
+            <p>${temperament}</p></div>`
+         
+        })
         .catch(err => {
             
             Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
                 position: 'center-top',
         
-            });
-         });
+            })
+                
+        });
         
-           
-        
- 
-elem.loader.classList.replace('loader', 'is-hidden');
-elem.select.classList.add('is-hidden');
-
-
-    
- }
-
-// function createMarcup(arr) {
-//     return arr.map(({url, breeds: {name,description,temperament}}) => `<img src = "${url}", alt = "${breeds[0].name}" width = "400"></>
-//     <h1>${breeds[0].name}</h1>
-//     <p>${breeds[0].description}</p>
-//     <h2>Temperament:</h2>
-//     <p>${breeds[0].temperament}</p>`).join('')
-
-// }
- //createMarcup()
-
-    
-    // elem.loader.classList.replace('loader', 'is-hidden');
-    //     elem.select.classList.remove('is-hidden');
-    //         const { url } = data[0];
-    //         console.log(data[0]);
-    //         const { name, description, temperament } = data[0].breeds[0];
-        
-    //     elem.cardCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${name}" width="400"/></div><div class="box"><h1>${name}</h1><p>${description}</p><p><b>Temperament:</b> ${temperament}</p></div>`
-    //     elem.cardCatInfo.classList.remove('is-hidden');
-    
-
-
+}
 
